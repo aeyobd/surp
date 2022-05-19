f3d3c6c9e11a938e8b96020cfbe5ff10a6dae173
Daniel Boyea <danielaboyea@gmail.com>
2022-05-19 08:16:47 -0400

    small updates

--- milkyway.py
+++ milkyway.py
@@ -5,14 +5,15 @@ from vice.toolkit import hydrodisk
 from VICE.migration.src.simulations.migration import diskmigration
 from VICE.migration.src.simulations.disks import star_formation_history
 import yields
+import gc
 
 # modify yields
 
 MAX_SF_RADIUS = 15.5 #kpc
 END_TIME = 13.2
 
-def run_model(name, spec="insideout", n_stars=2, agb_yields="cristallo11"):
-    dt = 0.01
+def run_model(name, spec="insideout", n_stars=2, agb_yields="cristallo11", seed=None, multithread=True):
+    dt = 0.01 #default 0.01
     zone_width = 0.1
     migration_mode = "diffusion"
     Nstars = min(2*MAX_SF_RADIUS/zone_width * END_TIME/dt * n_stars, 3102519)
@@ -29,21 +30,28 @@ def run_model(name, spec="insideout", n_stars=2, agb_yields="cristallo11"):
 
     model.elements = ("fe", "o", "c", "n")
     model.mode = "sfr"
-    model.setup_nthreads = 4
-    model.nthreads = 6
+    if multithread:
+        model.setup_nthreads = 6
+        model.nthreads = 4
+    else:
+        model.setup_nthreads = 1
+        model.nthreads = 1
     model.dt = dt
     model.bins = np.arange(-3, 3, 0.01)
             
 
     model.migration.stars = diskmigration(model.annuli,
-            N = n_stars, mode = migration_mode,
-            filename = "%s_analogdata.out" % (name))
+            N = Nstars, mode = migration_mode,
+            filename = "%s_analogdata.out" % (name),
+            seed = seed)
     model.evolution = star_formation_history(spec = spec,
             zone_width = zone_width)
 
 
     print(model)
-    model.run(np.arange(0, END_TIME, dt))
+    model.run(np.arange(0, END_TIME, dt), overwrite=True)
+    del model
+    gc.collect()
 
 
 if __name__ == "__main__":
@@ -52,8 +60,9 @@ if __name__ == "__main__":
     # run_model("karakas10", agb_yields="karakas10")
     # run_model("karakas10_lateburst", spec="lateburst", agb_yields="karakas10")
     # run_model("ventura13", agb_yields="ventura13")
-
     # run_model("ventura13_lateburst", agb_yields="ventura13", spec="lateburst")
     # run_model("karakas16", agb_yields="karakas16")
-    run_model("karakas16_lateburst", agb_yields="karakas16", spec="lateburst")
+    # run_model("karakas16_lateburst", agb_yields="karakas16", spec="lateburst")
+    # run_model("milkyway", seed=0, multithread=False)
+    run_model("milkyway_multithreaded", seed=0)
 
